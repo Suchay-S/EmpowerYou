c04cacb10c036ed72faf8b7fd2d0b6e9fc71a64e
Suchay-S <119693682+Suchay-S@users.noreply.github.com>
2023-08-08 13:11:39 -0500

    Add files via upload

---
+++ forum-firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA1t-dTXaPIj_ENf2Obvbde7Z3NvrtG8kk",
+    authDomain: "empower-you-914b7.firebaseapp.com",
+    databaseURL: "https://empower-you-914b7-default-rtdb.firebaseio.com",
+    projectId: "empower-you-914b7",
+    storageBucket: "empower-you-914b7.appspot.com",
+    messagingSenderId: "790894370305",
+    appId: "1:790894370305:web:1d3c4527ea9fc05a39ea00"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
